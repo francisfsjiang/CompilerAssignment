2890c57fcda5f5c2e644dd5f251d7fc634488e76
Neveralso <neveralso@gmail.com>
2014-12-01 14:04:31 +0800

    commit

--- scanner.py
+++ scanner.py
@@ -53,108 +53,12 @@ TOKEN_TAB = {
 }
 
 
-def get_token_by_id(id, value=0.):
-    token = Token(*TOKEN_TAB[id])
-    token.value = value
-    return token
-
-
 class Scanner():
     """
     """
     def __init__(self, file_name=None):
         file = open(file_name, mode='r')
-        self.stream = deque(file.read())
-        self.token_buffer = ''
-        self.line_no = 0
+        self.text = file.read()
+        self.re = re.compile(r'([a-zA-Z][a-zA-Z]*|[\d][\d]*[.[\d]*]?|\+|-|\*|/|\*\*|;|\(|\)|,)')
         file.close()
-
-    def _get_char(self):
-        if len(self.stream):
-        return self.stream.popleft()
-
-    def _back_char(self, char):
-        self.stream.appendleft(char)
-
-    def _add_token_buffer(self, char):
-        self.token_buffer += char
-
-    def _empty_token_buffer(self):
-        self.token_buffer = ''
-
-    def _is_eof(self):
-        if len(self.stream) == 0:
-            return True
-        else:
-            return False
-
-    def _judge_key_token(self, id):
-        if id in TOKEN_TAB:
-            return get_token_by_id(id)
-        return get_token_by_id('ERROR', value=self.line_no)
-
-    def _clean_stream(self):
-        """
-        clean the space, tab, enter, EOF in the stream
-        :return:
-        """
-        while True:
-            if self._is_eof():
-                return get_token_by_id('NONE')
-            char = self._get_char()
-            if char == '\n':
-                self.line_no += 1
-            if not char.isspace():
-                self._back_char(char)
-
-    def get_token(self):
-        self._empty_token_buffer()
-        char = self._get_char()
-        self._add_token_buffer(char)
-
-        if char.isalpha():  #ID
-            while True:
-                char = self._get_char()
-                if char.isalnum():
-                    self._add_token_buffer(char)
-                else:
-                    break
-            self._back_char(char)
-            return get_token_by_id(self.token_buffer)
-        elif char.isdigit():  #digit
-            while True:
-                char = self._get_char()
-                if char.isdigit():
-                    self._add_token_buffer(char)
-                else:
-                    break
-            if char == '.':
-                self._add_token_buffer(char)
-                while True:
-                    char = self._get_char()
-                    if char.isdigit():
-                        self._add_token_buffer(char)
-                    else:
-                        break
-            self._back_char(char)
-            return get_token_by_id('CONST', value=float(self.token_buffer))
-        else:  #operator
-            if char == ';':
-                return get_token_by_id('SEMICO')
-            elif char == '(':
-                return get_token_by_id('L_BRACKET')
-            elif char == ')':
-                return get_token_by_id('R_BRACKET')
-            elif char == ',':
-                return get_token_by_id('COMMMA')
-            elif char == '+':
-                return get_token_by_id('PLUS')
-            elif char == '-':
-                return get_token_by_id('MINUS')
-            elif char == '/':
-                char = self._get_char()
-                if char == '/'
-                    while char != '\n' and self.stream
-
-
 
